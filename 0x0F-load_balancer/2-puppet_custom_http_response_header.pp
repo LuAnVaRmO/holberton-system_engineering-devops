3f479c1c630d50ca1e69284619ee78bbdde2ef26
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-08-16 10:09:02 -0500

    advanced

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,22 @@
+# create task 0 but with puppet
+exec {'update':
+	command => 'sudo apt-get update',
+	provider => shell,
+}
+
+package {'nginx':
+	ensure => installed,
+}
+
+file_line {'header'
+	ensure => present,
+	path => '/etc/nginx/sites-available/default',
+	after => 'listen 80 default_server',
+	line => "add header X-Served_By ${hostname};",
+	require => Package['nginx'],
+}
+
+service {'nginx':
+	ensure => running,
+	require => File_line['header'],
+}
