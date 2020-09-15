dae3a4a1396a69ec21bd5996a7cd0aa4d5ff612b
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-09-14 22:14:13 -0500

    fix code

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,17 +3,6 @@
 import requests
 from sys import argv
 
-'''
-def print_completed_tasks(data):
-    total = len(data.get('tasks'))
-    completed_tasks = [t for t in data.get('tasks') if t.get('completed')]
-    completed = len(completed_tasks)
-    print('Employee {} is done with tasks({}/{}):'
-          .format(data.get('employee').get('name'), completed, total))
-    for task in completed_tasks:
-        print('\t {}'.format(task.get('title')))
-
-'''
 
 if __name__ == '__main__':
 
